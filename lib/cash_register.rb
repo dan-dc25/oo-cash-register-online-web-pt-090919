3d7b6e384eec7112982936477a0a98553b47f14b
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-14 01:08:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class CashRegister
 
+
+  def initialize(employee_discount = )
+    @total = 0
+  end
+
+end
+
+#add items of various quantities and prices
+#calculate discounts
+#keep track of what's been added to it
+#void the last transaction
